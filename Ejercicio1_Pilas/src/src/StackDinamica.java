0a448fb5d01f6a274e48314a059c978bfe7f5ad1
Ronald <ronaldnis.pilataxi@alu.uclm.es>
2025-10-28 18:11:13 +0100

    Dinamica a medias y excepcion creada

--- Ejercicio1_Pilas/src/src/StackDinamica.java
+++ Ejercicio1_Pilas/src/src/StackDinamica.java
@@ -1,5 +1,27 @@
 package src;
+import excepcionesPila.EmptyStackException;
 
-public class StackDinamica {
-
+public class StackDinamica<T> {
+	
+	private Node<T> top;
+	private int size;
+	
+	public StackDinamica() {
+		top = null;
+		size = 0;
+	}
+	
+	public T pop() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+		}
+		
+		T elemento = top.getElemento();
+		top = top.getNext();
+		size--;
+		return elemento;
+	}
+	public boolean isEmpty() {
+		return top == null;
+	}
 }
