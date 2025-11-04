a45d9fa25626160ebaa70b1a367a96b49749f060
Ronald <ronaldnis.pilataxi@alu.uclm.es>
2025-11-04 17:19:46 +0100

    Pila Dinamica completa

--- Ejercicio1_Pilas/src/src/StackDinamica.java
+++ Ejercicio1_Pilas/src/src/StackDinamica.java
@@ -10,7 +10,9 @@ public class StackDinamica<T> {
 		top = null;
 		size = 0;
 	}
-	
+	public int size() {
+		return size;
+	}
 	public T pop() {
 		if(isEmpty()) {
 			throw new EmptyStackException();
@@ -24,4 +26,18 @@ public class StackDinamica<T> {
 	public boolean isEmpty() {
 		return top == null;
 	}
+	
+	public void push(T elemento) {
+		Node <T> aux = new Node<>(elemento, top);
+		top=aux;
+		size++;
+	}
+	
+	public T top() {
+		if(isEmpty()) {
+			throw new EmptyStackException();
+			}
+		return top.getElemento();
+	}
+	
 }
