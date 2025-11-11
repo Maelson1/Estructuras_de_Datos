1dfef00dad109eb99c7106b875a375c7ea8a8089
kraskuut <pablogonzalezargudo@gmail.com>
2025-11-11 16:29:00 +0100

    Create CambioDeBase.java

---
+++ Ejercicio1_Pilas/src/src/CambioDeBase.java
@@ -0,0 +1,27 @@
+package Ejercicio1;
+
+public class CambioDeBase {
+	public static String convertirBase(int numero, int base, Stack<Integer> pila) {
+	    if (base < 2 || base > 9) {
+			
+		}
+
+	    if (numero == 0) {
+			return "0";
+		}
+
+	    while (numero > 0) {
+	        int residuo = numero % base;
+	        pila.push(residuo);
+	        numero /= base;
+	    }
+
+	    StringBuilder resultado = new StringBuilder();
+
+	    while (!pila.isEmpty()) {
+	        resultado.append(pila.pop());
+	    }
+
+	    return resultado.toString();
+	}
+}
