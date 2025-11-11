5132eccb8e10b816a83e4b857083f4a664e43509
kraskuut <pablogonzalezargudo@gmail.com>
2025-11-11 16:30:54 +0100

    Update StackEstatica.java

--- Ejercicio1_Pilas/src/src/StackEstatica.java
+++ Ejercicio1_Pilas/src/src/StackEstatica.java
@@ -1,34 +1,36 @@
-package src;		
+package Ejercicio1;
 
 import exceptions.FullArrayException;
 
-public abstract class StackEstatica<T> implements Stack<T>   {
+public class StackEstatica<T> implements Stack<T>   {
 
 
-	 
-	    private T[] stack;  
-	    private int N;      
-	    private int t;      
 
-	   
-	   
-		public void ArrayStack(int capacity) {
+	    private T[] stack;
+	    private int N;
+	    private int t;
+
+
+
+	    public StackEstatica(int capacity) {  
 	        N = capacity;
-	        stack = (T[]) new Object[N]; 
-	        t = -1; 
+	        stack = (T[]) new Object[N];
+	        t = -1;
 	    }
 
-        public void push(T value) throws FullArrayException {
+        @Override
+		public void push(T value) throws FullArrayException {
             if (t == N-1) {
 
                 System.out.println("Error: la pila está llena.");
                 return;
             }
-            stack[++t] = value; 
+            stack[++t] = value;
         }
 
 
-    public T pop() {
+    @Override
+	public T pop() {
         if(isEmpty()) {
             System.out.println("Error: la pila está vacía");
             return null;
@@ -39,30 +41,36 @@ public abstract class StackEstatica<T> implements Stack<T>   {
 
     }
 
-    public T top() {
-        if (isEmpty()) {  
+    @Override
+	public T top() {
+        if (isEmpty()) {
             System.out.println("Error: la pila está vacía");
-            return null; 
+            return null;
         } else {
-            return stack[t]; 
+            return stack[t];
         }
     }
 
 
-    public boolean isEmpty() {
+    @Override
+	public boolean isEmpty() {
         return t < 0;
 
     }
 
-    public int size() {
+    @Override
+	public int size() {
         return t + 1;
 
     }
-    public String toString() {
+    @Override
+	public String toString() {
         StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < N; i++) {
             sb.append(stack[i]);
-            if (i < N - 1) sb.append(", ");
+            if (i < N - 1) {
+				sb.append(", ");
+			}
         }
         sb.append("]");
         return sb.toString();
