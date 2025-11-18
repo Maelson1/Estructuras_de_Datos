4e3d513d291bb0522f2a251a24005d826a006cc6
kraskuut <pablogonzalezargudo@gmail.com>
2025-11-18 15:35:34 +0100

    Update TerminoDuplicado.java

--- Ejercicio3_Listas/exceptions/TerminoDuplicado.java
+++ Ejercicio3_Listas/exceptions/TerminoDuplicado.java
@@ -1,5 +1,10 @@
 package exceptions;
 
+/**
+ * Esta excepción sirve para cuando el usuario introduce un termino duplicado
+ * en el diccionario por lo tanto no es válido
+ * 
+ */
 public class TerminoDuplicado extends Exception {
     public TerminoDuplicado() {
         super("Error:El término ya existe.");
