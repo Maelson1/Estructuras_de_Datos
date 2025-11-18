a4ea34bee7441c174e50145ad15292a9dcea0256
kraskuut <pablogonzalezargudo@gmail.com>
2025-11-18 15:35:56 +0100

    Update TerminoNoEncontrado.java

--- Ejercicio3_Listas/exceptions/TerminoNoEncontrado.java
+++ Ejercicio3_Listas/exceptions/TerminoNoEncontrado.java
@@ -1,5 +1,11 @@
 package exceptions;
 
+/**
+ * Esta excepccion es usada para cuando el usuario introduce
+ * un término que no esta en el diccionario
+ * 
+ */
+
 public class TerminoNoEncontrado extends Exception {
     public TerminoNoEncontrado() {
         super("El término no está en el diccionario.");
