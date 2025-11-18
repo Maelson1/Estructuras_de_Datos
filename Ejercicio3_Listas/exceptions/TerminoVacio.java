f5936429ae8a747f02353a6cd3c9841b02452a12
kraskuut <pablogonzalezargudo@gmail.com>
2025-11-18 15:36:11 +0100

    Update TerminoVacio.java

--- Ejercicio3_Listas/exceptions/TerminoVacio.java
+++ Ejercicio3_Listas/exceptions/TerminoVacio.java
@@ -1,5 +1,10 @@
 package exceptions;
 
+/*
+ * Esta excepccion es usada para cuando el usuario introduce un
+ * término vacío
+ * 
+ */
 public class TerminoVacio extends Exception {
 	  public TerminoVacio() {
 	        super("Error:El término no puede estar vacío.");
